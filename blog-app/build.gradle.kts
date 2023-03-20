7682cd92104d1817f6a4a8d2e182f00400b0ada6
sungho <tjdgh1231@tpirates.com>
2023-03-21 01:16:49 +0900

    chore : 주석 추가

--- blog-app/build.gradle.kts
+++ blog-app/build.gradle.kts
@@ -10,20 +10,28 @@ plugins {
 val ktorVersion="2.1.3"
 
 dependencies {
+	// module
 	implementation(project(":blog-app-domain"))
 	implementation(project(":blog-app-data"))
 	implementation(project(":blog-app-external-api"))
+
+	// spring
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+	// kotlin
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 
-
+	// ktor
 	implementation("io.ktor:ktor-client-core:$ktorVersion")
 	implementation("io.ktor:ktor-client-cio:$ktorVersion")
 	implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
 	implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 
+	// test
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.kotest:kotest-runner-junit5:${rootProject.extra.get("kotestVersion")}")
 	testImplementation("io.kotest:kotest-assertions-core:${rootProject.extra.get("kotestVersion")}")
