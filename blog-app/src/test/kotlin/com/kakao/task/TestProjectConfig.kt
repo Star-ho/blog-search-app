30ad4897acae7f22499b8e2428d459b050307b81
sungho <tjdgh1231@tpirates.com>
2023-03-20 18:19:03 +0900

    feat : IsolationMode 추가

--- blog-app/src/test/kotlin/com/kakao/task/TestProjectConfig.kt
+++ blog-app/src/test/kotlin/com/kakao/task/TestProjectConfig.kt
@@ -1,8 +1,10 @@
-package com.kakao.task.data.h2
+package com.kakao.task
 
 import io.kotest.core.config.AbstractProjectConfig
+import io.kotest.core.spec.IsolationMode
 import io.kotest.extensions.spring.SpringExtension
 
 class ProjectConfig : AbstractProjectConfig() {
     override fun extensions() = listOf(SpringExtension)
+    override fun isolationMode() = IsolationMode.InstancePerTest
 }
