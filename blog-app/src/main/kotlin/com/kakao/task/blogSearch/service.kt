6c68fecd77c87759b55ca5cf4c5271732d945a16
sungho <tjdgh1231@tpirates.com>
2023-03-20 22:22:12 +0900

    chore : 파일 분리

---
+++ blog-app/src/main/kotlin/com/kakao/task/blogSearch/service.kt
@@ -0,0 +1,29 @@
+package com.kakao.task.blogSearch
+
+import com.kakao.task.domain.searchKeywordLog.SearchKeywordLog
+import com.kakao.task.domain.searchKeywordLog.SearchKeywordLogRepository
+import com.kakao.task.externalApi.BlogSearchResponse
+import com.kakao.task.externalApi.KakaoBlogSearch
+import com.kakao.task.externalApi.NaverBlogSearch
+import com.kakao.task.externalApi.SearchRequest
+import org.springframework.stereotype.Service
+
+@Service
+class BlogSearchService(
+        private val kakaoBlogSearch: KakaoBlogSearch,
+        private val naverBlogSearch: NaverBlogSearch,
+        private val searchKeywordLogRepository: SearchKeywordLogRepository,
+) {
+    fun getData(searchRequest: SearchRequest): BlogSearchResponse? {
+        val res = try{ kakaoBlogSearch.getBlogData(searchRequest) }
+        catch (e:Exception){naverBlogSearch.getBlogData(searchRequest)}
+
+        try {
+            searchKeywordLogRepository.save(SearchKeywordLog(keyword = searchRequest.query))
+        }catch (e:Exception){
+            e.printStackTrace()
+        }
+
+        return res
+    }
+}
