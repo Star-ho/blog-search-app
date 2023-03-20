422316a3e3424a0a558f64160e71fbf41070875b
sungho <tjdgh1231@tpirates.com>
2023-03-20 23:34:30 +0900

    fix: test환경에서 스케줄러 돌어가지 않도록 수정

--- blog-app/src/main/kotlin/com/kakao/task/BlogAppApplication.kt
+++ blog-app/src/main/kotlin/com/kakao/task/BlogAppApplication.kt
@@ -2,10 +2,9 @@ package com.kakao.task
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.scheduling.annotation.EnableScheduling
+
 
 @SpringBootApplication
-@EnableScheduling
 class BlogAppApplication
 
 fun main(args: Array<String>) {
