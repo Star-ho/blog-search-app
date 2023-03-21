df90d7c56034348931afd038c66e1275f4b4def0
sungho <tjdgh1231@tpirates.com>
2023-03-22 01:46:39 +0900

    fix : 버전 변수 추출

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    val kotlinPluginVersion = "1.7.22"
     id("org.springframework.boot") version "3.0.4" apply false
     id("io.spring.dependency-management") version "1.1.0" apply false
 
-    kotlin("plugin.serialization") version "1.7.22" apply false
-    kotlin("jvm") version "1.7.22" apply false
-    kotlin("plugin.spring") version "1.7.22" apply false
-    kotlin("plugin.jpa") version "1.7.22" apply false
+    kotlin("plugin.serialization") version kotlinPluginVersion apply false
+    kotlin("jvm") version kotlinPluginVersion apply false
+    kotlin("plugin.spring") version kotlinPluginVersion apply false
+    kotlin("plugin.jpa") version kotlinPluginVersion apply false
 }
 
 group = "com.kakao"
