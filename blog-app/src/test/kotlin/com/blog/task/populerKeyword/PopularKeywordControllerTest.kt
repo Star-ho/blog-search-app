082fb78d70a83ed291a9c3189737564f79b7ac37
sungho <tjdgh1231@tpirates.com>
2023-03-22 19:05:35 +0900

    fix : webclient로 변경

--- blog-app/src/test/kotlin/com/blog/task/populerKeyword/PopularKeywordControllerTest.kt
+++ blog-app/src/test/kotlin/com/blog/task/populerKeyword/PopularKeywordControllerTest.kt
@@ -2,19 +2,22 @@ package com.blog.task.populerKeyword
 
 
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.matchers.shouldBe
-import io.ktor.client.*
-import io.ktor.client.request.*
-import io.ktor.http.*
+import io.kotest.matchers.booleans.shouldBeTrue
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.reactive.function.client.WebClient
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class PopularKeywordControllerTest : FunSpec({
     test("블로그 검색 api test"){
-        val client = HttpClient()
-
-        client.get("http://localhost:8080/popular-keyword").let {
-            it.status shouldBe HttpStatusCode.OK
-        }
+        WebClient.create().get()
+            .uri("http://localhost:8080/popular-keyword")
+            .accept(MediaType.APPLICATION_JSON)
+            .retrieve()
+            .toEntity(ResponseEntity::class.java)
+            .subscribe{
+                it.statusCode.is2xxSuccessful.shouldBeTrue()
+            }
     }
 })
