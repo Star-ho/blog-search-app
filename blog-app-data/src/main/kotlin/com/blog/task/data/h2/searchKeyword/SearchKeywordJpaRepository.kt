914ccdcdffd1ce3056ed014b0ccf05697c6bf98b
sungho <tjdgh1231@tpirates.com>
2023-03-22 19:03:55 +0900

    fix : 어노테이션 추가

--- blog-app-data/src/main/kotlin/com/blog/task/data/h2/searchKeyword/SearchKeywordJpaRepository.kt
+++ blog-app-data/src/main/kotlin/com/blog/task/data/h2/searchKeyword/SearchKeywordJpaRepository.kt
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
+import org.springframework.stereotype.Repository
 import java.math.BigDecimal
 
 @Entity
@@ -32,6 +33,7 @@ class SearchKeywordDao(
         }
 }
 
+@Repository
 interface SearchKeywordJpaRepository: JpaRepository<SearchKeywordDao, Long>, JpaSpecificationExecutor<SearchKeywordDao>{
 
         @Query("select SKW from SearchKeywordDao SKW order by SKW.hitCount desc limit 10")
