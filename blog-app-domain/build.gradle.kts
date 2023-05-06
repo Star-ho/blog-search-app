f50470b36a97c85942bba38453bcface987e6c98
sungho <tjdgh1231@tpirates.com>
2023-05-06 15:37:09 +0900

    fix : 코틀린 라이브러리 추가

--- blog-app-domain/build.gradle.kts
+++ blog-app-domain/build.gradle.kts
@@ -3,6 +3,10 @@ plugins {
 }
 
 dependencies{
+    // kotlin
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:${rootProject.extra.get("ktorVersion")}")
+
     testImplementation("io.kotest:kotest-runner-junit5:${rootProject.extra.get("kotestVersion")}")
     testImplementation("io.kotest:kotest-assertions-core:${rootProject.extra.get("kotestVersion")}")
 }
